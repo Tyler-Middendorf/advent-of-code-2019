d96410bf380b27bd9a0cc8d69de9201bc937e8d6
Tyler Middendorf <34323666+Tyler-Middendorf@users.noreply.github.com>
2019-12-03 00:31:42 -0600

    Create Cord.java

---
+++ Day 3/Cord.java
@@ -0,0 +1,22 @@
+package gfujsef;
+
+public class Cord{
+	private int x;
+	private int y;
+	public Cord(int x,int y) {
+		this.x=x;
+		this.y=y;
+	}
+	public int getx() {
+		return x;
+	}
+	public int gety() {
+		return y;
+	}
+	public String toString() {
+		return x+" "+y;
+	}
+	
+	
+	
+}
